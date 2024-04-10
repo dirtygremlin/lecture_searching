b9a4c8b5d79f4e41091be0a5033b029fc86e71f5
Vladyslav Hnitii <256853@vutbr.cz>
2024-04-10 09:35:09 +0200

    1

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,10 +13,28 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path) as data:
+        content = json.load(data)
+    if field in content.keys():
+        return content[field]
+    else:
+        return None
+
+
+def linear_search(sequence, num):
+    list = []
+    for idx, i in enumerate(sequence):
+        if i == num:
+            list.append(idx)
+    result = dict()
+    result["positions"] = list
+    result["count"] = len(list)
+    return result
 
 
 def main():
-    pass
+
+    return
 
 
 if __name__ == '__main__':
